f5d6bcda4b2a89d640ca15c0e9e8dd357df88391
St0pien <jstepien50@gmail.com>
2024-03-14 10:50:53 +0100

    Add vitest for unit testing, update README

---
+++ client/src/lib/utils.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest';
+
+test('Example test', () => {
+  expect('hello ' + 'world').toBe('hello world');
+});
---
+++ server/src/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from 'vitest';
+
+describe('Server', () => {
+    test('Example', () => {
+        expect(2 + 2).toBe(4);
+    });
+});
