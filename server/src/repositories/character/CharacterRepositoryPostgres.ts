492e12d13cc3a7d22d3516270f7e296b4cffa379
St0pien <jstepien50@gmail.com>
2024-04-13 15:41:12 +0200

    Change directory naming, add initial migration

--- server/src/repositories/character/CharacterRepositoryPostgres.ts
+++ server/src/repositories/character/CharacterRepositoryPostgres.ts
@@ -1,13 +1,20 @@
 import { UUID } from 'crypto';
+import { Kysely } from 'kysely';
 
 import { Character } from '@/domain/game/Character';
+import { Database } from '@/infrastructure/postgres/db';
 
 import { ICharacterRepository } from './ICharacterRepository';
 
 /**
- * Postgres implementation of {@link ICharacterRepository}
+ * Postgres based implementation of {@link ICharacterRepository}
  */
 export class CharacterRepositoryPostgres implements ICharacterRepository {
+    constructor(db: Kysely<Database>) {
+        db;
+        Kysely;
+    }
+
     /**
      * creates character entry in postgres and returns it's {@link Character} instance
      */
--- server/src/repositories/user/UserRepositoryPostgres.ts
+++ server/src/repositories/user/UserRepositoryPostgres.ts
@@ -1,8 +1,14 @@
+import { Kysely } from 'kysely';
+
 import { User } from '@/domain/game/User';
+import { Database } from '@/infrastructure/postgres/db';
 
 import { IUserRepository } from './IUserRepository';
 
 export class UserRepositoryPostgres implements IUserRepository {
+    constructor(db: Kysely<Database>) {
+        db;
+    }
     createUser(): User {
         return new User();
     }
