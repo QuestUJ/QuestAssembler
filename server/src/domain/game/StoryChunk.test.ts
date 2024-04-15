2308fb756eacff064a7374b2d59a8232730706b1
wouchan <axamber0@gmail.com>
2024-04-15 20:02:18 +0200

    Implement tests for StoryChunk

---
+++ server/src/domain/game/StoryChunk.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, it } from 'vitest';
+
+import { StoryChunk } from './StoryChunk';
+
+describe('StoryChunk contruction', () => {
+    const id = 0;
+    const title = 'Test title!';
+    const content = 'Test content!';
+    const imageUrl = 'Test image url!';
+    const timestamp = new Date();
+
+    const storyChunk = new StoryChunk(id, title, content, imageUrl, timestamp);
+
+    it('Persists message content', () => {
+        expect(storyChunk.id).toEqual(id);
+        expect(storyChunk.title).toEqual(title);
+        expect(storyChunk.content).toEqual(content);
+        expect(storyChunk.imageUrl).toEqual(imageUrl);
+        expect(storyChunk.timestamp).toEqual(timestamp);
+    });
+});
