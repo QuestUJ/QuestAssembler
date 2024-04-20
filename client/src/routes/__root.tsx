a2cfbdcc0f6c950dbff415a3f1cc9fc90955fbeb
Weronika Zygis <115404427+werka-z@users.noreply.github.com>
2024-04-21 00:37:12 +0200

    Add Sidebar (drawer) and QuasmCollapsible components, but with terrible styling and hard coded parts

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { Auth0Provider } from '@auth0/auth0-react';
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 
+import { Sidebar } from '@/components/Sidebar';
 import { User } from '@/components/User';
 import { config } from '@/config';
 import { SocketIOProvider } from '@/providers/SocketIOProvider';
@@ -25,20 +26,23 @@ function RootLayout() {
         }}
       >
         <SocketIOProvider>
-          <nav className='absolute flex w-full items-center justify-end p-4 px-32'>
-            <Link to='/' className='mx-4 text-primary'>
-              Home
-            </Link>
-            <div>|</div>
-            <Link to='/rooms' className='mx-4'>
-              Rooms
-            </Link>
-            <div>|</div>
-            <User />
-          </nav>
-          <main className='min-h-screen w-screen'>
-            <Outlet />
-          </main>
+          <Sidebar />
+          <div className='flex'>
+            <nav className='absolute flex w-full items-center justify-end p-4 px-32'>
+              <Link to='/' className='mx-4 text-primary'>
+                Home
+              </Link>
+              <div>|</div>
+              <Link to='/rooms' className='mx-4'>
+                Rooms
+              </Link>
+              <div>|</div>
+              <User />
+            </nav>
+            <main className='min-h-screen w-screen'>
+              <Outlet />
+            </main>
+          </div>
         </SocketIOProvider>
       </Auth0Provider>
     </>
