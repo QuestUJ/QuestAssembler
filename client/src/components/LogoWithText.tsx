16a092d8857d3cdd1e226401497a8783b8ca220b
avgmathenjoyer <avgmathenjoyer@gmail.com>
2024-04-24 16:17:56 +0200

    add landing page

---
+++ client/src/components/LogoWithText.tsx
@@ -0,0 +1,14 @@
+import { Link } from '@tanstack/react-router';
+
+import logoSVG from '/public/logo.svg';
+
+export default function LogoWithText() {
+  return (
+    <Link to='/'>
+      <div className='flex h-20 w-80 flex-row items-center'>
+        <img src={logoSVG} alt='Quasm logo' className='h-20 text-primary' />
+        <h1 className='font-medieval text-5xl text-primary'>Quasm</h1>
+      </div>
+    </Link>
+  );
+}
