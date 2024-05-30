2aeedb2cf779727e83a6e9b5933925d7230061cf
avgmathenjoyer <avgmathenjoyer@gmail.com>
2024-05-30 17:04:25 +0200

    add different ways of displaying nicknames, fix #47

---
+++ client/src/lib/misc/displayNickname.ts
@@ -0,0 +1,35 @@
+// make nickname shorter according to viewport size
+export enum NicknameDisplayStyle {
+  LONG = 'LONG',
+  SHORT = 'SHORT'
+}
+
+const SMALL_SCREEN_MAX_WIDTH = 480;
+const MEDIUM_SCREEN_MAX_WIDTH = 768;
+
+const LONG_DISPLAY_SMALL_SCREEN_NICK_LENGTH = 20;
+const LONG_DISPLAY_MEDIUM_SCREEN_NICK_LENGTH = 32;
+
+const SHORT_DISPLAY_LONG_NICKNAME_LENGTH = 13;
+
+export function displayNickname(
+  nick: string,
+  style: NicknameDisplayStyle,
+  screenWidth?: number
+) {
+  if (style == NicknameDisplayStyle.LONG) {
+    if (screenWidth && screenWidth < SMALL_SCREEN_MAX_WIDTH) {
+      return nick.substring(0, LONG_DISPLAY_SMALL_SCREEN_NICK_LENGTH) + '...';
+    } else if (screenWidth && screenWidth < MEDIUM_SCREEN_MAX_WIDTH) {
+      return nick.substring(0, LONG_DISPLAY_MEDIUM_SCREEN_NICK_LENGTH) + '...';
+    } else {
+      return nick;
+    }
+  } else {
+    if (nick.length > SHORT_DISPLAY_LONG_NICKNAME_LENGTH) {
+      return nick.substring(0, SHORT_DISPLAY_LONG_NICKNAME_LENGTH - 3) + '...';
+    } else {
+      return nick;
+    }
+  }
+}
